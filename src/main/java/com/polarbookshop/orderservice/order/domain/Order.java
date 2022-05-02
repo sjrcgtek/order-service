598a8612d4728c2fa2d0e464b734017ad9ca98e8
Steve Rogers <steve.cgtek@gmail.com>
2022-05-01 23:01:55 -0500

    ch 12

--- src/main/java/com/polarbookshop/orderservice/order/domain/Order.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/Order.java
@@ -2,8 +2,10 @@ package com.polarbookshop.orderservice.order.domain;
 
 import java.time.Instant;
 
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.relational.core.mapping.Table;
@@ -11,27 +13,33 @@ import org.springframework.data.relational.core.mapping.Table;
 @Table("orders")
 public record Order (
 
-	@Id
-	Long id,
+		@Id
+		Long id,
 
-	String bookIsbn,
-	String bookName,
-	Double bookPrice,
-	Integer quantity,
-	OrderStatus status,
+		String bookIsbn,
+		String bookName,
+		Double bookPrice,
+		Integer quantity,
+		OrderStatus status,
 
-	@CreatedDate
-	Instant createdDate,
+		@CreatedDate
+		Instant createdDate,
 
-	@LastModifiedDate
-	Instant lastModifiedDate,
+		@LastModifiedDate
+		Instant lastModifiedDate,
 
-	@Version
-	int version
+		@CreatedBy
+		String createdBy,
+
+		@LastModifiedBy
+		String lastModifiedBy,
+
+		@Version
+		int version
 ){
 
 	public static Order build(String bookIsbn, String bookName, Double bookPrice, Integer quantity, OrderStatus status) {
-		return new Order(null, bookIsbn, bookName, bookPrice, quantity, status, null, null, 0);
+		return new Order(null, bookIsbn, bookName, bookPrice, quantity, status, null, null, null, null, 0);
 	}
 
 }
